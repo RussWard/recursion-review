ac6dcb82ad3429ee257b851bb6a30bf201ebc8ed
MKS-Student <mks.student@hackreactor.com>
2017-03-20 11:54:55 -0700

    Make tests pass for getElementsByClassName and update test file.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+  var checkElement = function (element) {
+    if(_.contains(element.classList, className)) {
+      results.push(element);
+    }
+    _.each(element.childNodes, function (node) { 
+      checkElement(node);
+    });
+  };
+  checkElement(document.body); 
+  return results;
 };
