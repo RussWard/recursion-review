9df52b508d7c9d77b26b893a86c1d421a4503d82
MKS-Student <mks.student@hackreactor.com>
2017-03-20 13:09:26 -0700

    Work in progress on stringifyJSON, passin up to unstringifiable values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,30 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //var strings = [];
+  if (obj === null) {
+    return 'null';
+  } else if (obj === undefined || typeof obj === 'function') {
+    return;
+  } else if (typeof obj === 'string') {
+    return "\"" + obj + "\"";
+  } else if (Array.isArray(obj)) {
+    var arrayContents = obj.map(function(element) {
+      return stringifyJSON(element);
+    });
+    return '[' + arrayContents.join(',') + ']';  
+  } else if (obj.constructor === Object) {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      var objectContents = Object.keys(obj).map(function(key) {
+        if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+          return stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        }
+      });
+      return '{' + objectContents.join(',') + '}';
+    } 
+  } else {
+    return obj.toString();
+  }
 };
