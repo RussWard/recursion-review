4d453ea6ce3546818d966b351a1672847e9a203c
MKS-Student <mks.student@hackreactor.com>
2017-03-20 20:03:56 -0700

    Begin work on parseJson

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,44 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  var parseValue = function(json) {
+    var result;
+    if(json === 'true') {
+      result = true;
+    }
+    if(json === 'false') {
+      result = false;
+    }
+    if(json === 'null') {
+      result = null;
+    }
+    if(parseInt(json) !== NaN) {
+      result = parseInt(json);
+    }
+    if(json[0] === '[') {
+      result = parseArray(json);
+    }
+    if(json[0] === '{') {
+      result = parseObject(json);
+    } else {
+      result = json.slice(1, -1)
+    }
+    return result;
+  }
+  var parseArray = function(json) {
+    var result = [];
+    if(json !== '[]') {
+      json.slice(1, json.lastIndexOf(']')).split(',').forEach(function(element) {
+        result.push(parseValue(element));
+      });
+    }
+    return result;
+  }
+  var parseObject = function(json) {
+    var result = {};
+    if(json !== '{}') {
+      
+    }
+    return result;
+  }
 };
